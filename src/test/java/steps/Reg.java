5295f76bb65246f4468dafabfcfd7fad662b19f1
FakeToggle <worthytrying@gmail.com>
2026-01-03 18:58:45 +0100

    add

--- src/test/java/steps/Reg.java
+++ src/test/java/steps/Reg.java
@@ -1,15 +1,12 @@
 package steps;
 
-import com.sun.source.tree.AssertTree;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
